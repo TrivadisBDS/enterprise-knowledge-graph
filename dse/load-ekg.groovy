b6d48dc258a21f1907e8d644427bd6f221d85018
Guido Schmutz <guido.schmutz@hotmail.com>
2016-10-03 23:22:10 +0200

    add model and additional data files

--- dse/load-ekg.groovy
+++ dse/load-ekg.groovy
@@ -17,7 +17,13 @@ config create_schema: true, load_new: true, load_threads: 3
 inputfiledir = '/home/marco/enterprise-knowledge-graph/dse/csv/'
 employeeInput = File.csv(inputfiledir + "employee2.csv").delimiter(',')
 termInput = File.csv(inputfiledir + "term2.csv").delimiter(',')
+presentationInput = File.csv(inputfiledir + "presentation2.csv").delimiter(',')
+certificationInput = File.csv(inputfiledir + "certification2.csv").delimiter(',')
+
 employeeTermInput = File.csv(inputfiledir + "employeeTerm2.csv").delimiter(',')
+employeePresentationInput = File.csv(inputfiledir + "employeePresentation2.csv").delimiter(',')
+employeeCertificationInput = File.csv(inputfiledir + "employeeCertification2.csv").delimiter(',')
+certificationTermInput = File.csv(inputfiledir + "certificationTerm2.csv").delimiter(',')
 
 //Specifies what data source to load using which mapper (as defined inline)
   
@@ -31,10 +37,56 @@ load(termInput).asVertices {
     key "Name"
 }
 
+load(presentationInput).asVertices {
+    label "Presentation"
+    key "Name"
+}
+
+load(certificationInput).asVertices {
+    label "Certification"
+    key "Name"
+}
+
 load(employeeTermInput).asEdges {
     label "skill"
     outV "eName", {
         label "Employee"
+        key "Name"
+    }
+    inV "tName", {
+        label "Term"
+        key "Name"
+    }
+}
+
+load(employeePresentationInput).asEdges {
+    label "participates in"
+    outV "eName", {
+        label "Employee"
+        key "Name"
+    }
+    inV "pName", {
+        label "Presentation"
+        key "Name"
+    }
+}
+
+load(employeeCertificationInput).asEdges {
+    label "owns"
+    outV "eName", {
+        label "Employee"
+        key "Name"
+    }
+    inV "cName", {
+        label "Certification"
+        key "Name"
+    }
+}
+
+load(certificationTermInput).asEdges {
+    label "related to"
+    outV "cName", {
+        label "Certification"
         key "Name"
     }
     inV "tName", {
